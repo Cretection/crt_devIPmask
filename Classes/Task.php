b9352d2b0b482f0617fd9f824a6271ad7a901d34
Cretection <info@cretection.eu>
2015-05-02 12:55:13 +0200

    New Test

---
+++ Classes/Task.php
@@ -0,0 +1,10 @@
+<?php
+   namespace TYPO3\Mytask\Task;
+   
+   class Task extends \TYPO3\CMS\Scheduler\Task\AbstractTask {
+      public function execute() {
+         //dein code mit fehlerabfragen etc.
+         return TRUE; //ende gut, alles gut?
+      }
+   }
+?>
--- ext_autoload.php
+++
@@ -1,5 +0,0 @@
-<?php
-/*return array(
-    'tx_crtdevipmask_getip' => \TYPO3\CMS\Core\Utility\GeneralManagementUtility::extPath('crt_devipmask', 'tasks/class.tx_crtdevipmask_getip.php')
-);*/
-?>
