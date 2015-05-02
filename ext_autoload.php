064ccefcef39c6be57990997357f9af277c4ed93
Cretection <info@cretection.eu>
2015-05-02 12:39:00 +0200

    Bugfix

--- ext_autoload.php
+++ ext_autoload.php
@@ -1,5 +1,5 @@
 <?php
 return array(
-    'tx_crtdevipmask_getip' => \TYPO3\CMS\Core\Utility\GeneralManagementUtility::extPath('sfpinger', 'tasks/class.tx_sfpinger_pinger.php')
+    'tx_crtdevipmask_getip' => \TYPO3\CMS\Core\Utility\GeneralManagementUtility::extPath('crt_devipmask', 'tasks/class.tx_crtdevipmask_getip.php')
 );
 ?>
