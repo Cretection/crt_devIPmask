47796b51dba7339215dff150526ea50527fdf228
Cretection <info@cretection.eu>
2015-05-02 12:44:30 +0200

    Fix?

--- ext_autoload.php
+++ ext_autoload.php
@@ -1,5 +1,5 @@
 <?php
 return array(
-    'tx_crtdevipmask_getip' => \TYPO3\CMS\Core\Utility\GeneralManagementUtility::extPath('crt_devipmask', 'tasks/class.tx_crtdevipmask_getip.php')
+    'tx_crtdevipmask_getip' => \TYPO3\CMS\Core\Utility\GeneralManagementUtility::extPath('crt_devipmask', 'tasks/class.tx_crtdevipmask_getip.php');
 );
 ?>
