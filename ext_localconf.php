72c9716cd438f2099a1406cb159b3785332e656d
Cretection <info@cretection.eu>
2015-05-02 15:49:56 +0200

    Added

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,7 +5,7 @@ if (!defined('TYPO3_MODE')) {
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['TYPO3\\CrtDevipmask\\Task\\Task'] = array(
     'extension'        => $_EXTKEY,
-    'title'            => 'Server anpingen',
+    'title'            => 'DevIPmask',
     'description'      => 'Dieser Task benötigt eine IP-Adresse, die er alle 15 Minuten anpingen kann.',
 );
 ?>
