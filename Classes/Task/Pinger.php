da8f9c1ff86b3fbb3733badc7d11c217609d1cb3
Cretection <info@cretection.eu>
2015-05-05 01:23:34 +0200

    SQL Test 3

--- Classes/Task/Pinger.php
+++ Classes/Task/Pinger.php
@@ -37,7 +37,13 @@ class Pinger extends \TYPO3\CMS\Scheduler\Task\AbstractTask {
 			'lastip' => $ip[0],
 		);
 		$uidCheck = $GLOBALS['TYPO3_DB']->exec_SELECTgetRows('uid', 'tx_crtdevipmask_ip','uid = 1');
-		var_dump($uidCheck);
+		var_dump($uidCheck)
+		if($uidCheck){
+			echo('Yes');
+		}else {
+			echo('No');
+		}
+		
 		/**if(){
 			$res = $GLOBALS['TYPO3_DB']->exec_UPDATEquery('tx_crtdevipmask_ip','uid=1', $ipInsert);
 		} else {
